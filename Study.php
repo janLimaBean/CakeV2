5d5ec585c8cf35ca3c4bc455b15881746497e9b1
JJ Swanevelder <Jannie.swanevelder@gmail.com>
2017-02-12 21:40:53 +0200

    Added Study.php File - used to track course

---
+++ Study.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Study GuideLine</title>
+</head>
+<body>
+
+	<h1>Study guideline</h1>
+	<ul><em>Additional Resources to read</em>
+		<li><a href="https://book.cakephp.org/2.0/en/getting-started/cakephp-conventions.html">Cake Naming Conventions</a>
+		</li>
+	</ul>
+	<ul><em>Course files</em>
+		<li><a href="https://book.cakephp.org/2.0/en/tutorials-and-examples/blog/part-two.html">Create a Post Model</a></li>
+	</ul>
+</body>
+</html>
